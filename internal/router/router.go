cf69603a8cb0eb45ef5a880126dd3f7874559ac8
Vung Nguyen <vungvung1001@gmail.com>
2025-03-30 18:36:30 +0700

    add home page, ping

--- internal/router/router.go
+++ internal/router/router.go
@@ -9,7 +9,26 @@ import (
 // SetupRouter initializes and returns the router
 func SetupRouter() *gin.Engine {
 	r := gin.Default()
-	r.GET("/players", controller.GetPlayers)
-	r.POST("/update-score", controller.UpdateScore)
+
+	// Load HTML templates from the "templates" directory
+	r.LoadHTMLGlob("templates/*")
+
+	r.GET("/", func(c *gin.Context) {
+		// Render the "index.html" file
+		c.HTML(200, "index.html", nil)
+	})
+	// Group API routes under /api/v1
+	api := r.Group("/api/v1")
+	{
+		api.POST("/ping", func(c *gin.Context) {
+			message := c.DefaultQuery("message", "pong")
+			c.JSON(200, gin.H{
+				"message": message,
+			})
+		})
+		api.GET("/players", controller.GetPlayers)
+		api.POST("/update-score", controller.UpdateScore)
+	}
+
 	return r
 }
